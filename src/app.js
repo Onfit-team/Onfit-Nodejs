6fcf7289703eea5373cb95340584ec6194c17490
kim-yebeen <kimyebeen125@gmail.com>
2025-07-11 02:39:44 +0900

    [fix] 충돌 해결 및 dotenv 수정

--- src/app.js
+++ src/app.js
@@ -1,18 +1,23 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from 'express';
 import passport from './modules/user/kakao.strategy.js'; // 초기화
 import userRoutes from './routes/user.route.js';
-import dotenv from "dotenv";
+import homeRouter from './routes/home.route.js';
 import { errorHandler } from './middlewares/error.middleware.js';
 
-dotenv.config();
+
 const app = express();
 app.use(express.json());
 app.use(passport.initialize());
 
 app.use('/user', userRoutes);
+app.use(homeRouter);
+
+app.use(errorHandler); // 라우터 맨 아래에 추가
 
 app.listen(3000, () => {
   console.log('Server running on http://localhost:3000');
 });
 
-app.use(errorHandler); // 라우터 맨 아래에 추가
