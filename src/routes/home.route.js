bce5bf579249486db9a9c587f80c5fddcee56e98
kim-yebeen <kimyebeen125@gmail.com>
2025-07-11 02:39:44 +0900

    [fix] 충돌 해결 및 dotenv 수정

--- src/routes/home.route.js
+++ src/routes/home.route.js
@@ -1,8 +1,12 @@
-const express = require('express');
-const router = express.Router();
-const homeController = require('../modules/home/home.controller');
-//const authMiddleware = require('../../middlewares/auth.middleware');
+import express from 'express';
+import { getCurrentDateController } from '../modules/home/home.controller.js';
+//import authenticateJWT from '../middlewares/auth.middleware.js';
 
-//router.get('/common/date', authMiddleware, homeController.getCurrentDate);
-router.get('/common/date', homeController.getCurrentDate);
-module.exports = router;
+const router = express.Router();
+
+//router.get('/common/date', authenticateJWT, getCurrentDateController);
+router.get('/common/date', getCurrentDateController);
+
+
+
+export default router;
