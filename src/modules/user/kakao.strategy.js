3821bf96feadfd73c19fa968d542d748bdbb4a3f
chaeyeon089 <chaeyeon0091@naver.com>
2025-07-10 15:07:59 +0900

    #6 [feat]: 카카오 소셜로그인

---
+++ src/modules/user/kakao.strategy.js
@@ -0,0 +1,22 @@
+import passport from 'passport';
+import { Strategy as KakaoStrategy } from 'passport-kakao';
+import { userService } from './user.service.js';
+
+passport.use(
+  new KakaoStrategy(
+    {
+      clientID: process.env.KAKAO_CLIENT_ID,
+      callbackURL: process.env.KAKAO_CALLBACK_URL
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        const result = await userService.loginWithKakao(profile);
+        done(null, result); // { token, user }
+      } catch (err) {
+        done(err);
+      }
+    }
+  )
+);
+
+export default passport;
