7634ebbbef29049f8a1c60f2cae555ddb342e51d
chaeyeon089 <chaeyeon0091@naver.com>
2025-07-10 21:46:01 +0900

    #8 [fix]: 공통응답, 에러처리

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -2,23 +2,20 @@ import express from 'express';
 import passport from '../modules/user/kakao.strategy.js';
 import authenticateJWT from '../middlewares/auth.middleware.js';
 
-import { updateNickname } from '../modules/user/user.controller.js';
+import { updateNickname, kakaoCallback } from '../modules/user/user.controller.js';
 
 const router = express.Router();
 
-// ✅ 카카오 로그인
+// 카카오 로그인
 router.get('/auth/kakao', passport.authenticate('kakao'));
 
 router.get(
   '/auth/kakao/callback',
   passport.authenticate('kakao', { session: false }),
-  (req, res) => {
-    const { token, user } = req.user;
-    res.json({ token, user });
-  }
+  kakaoCallback
 );
 
-// ✅ 닉네임 수정 (JWT 필요)
+// 닉네임 수정 (JWT 필요)
 router.patch('/auth/signup', authenticateJWT, updateNickname);
 
 export default router;
