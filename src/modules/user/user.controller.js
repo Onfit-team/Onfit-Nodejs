7a2842a7769c8c60f1599c25462d69ab56289f40
chaeyeon089 <chaeyeon0091@naver.com>
2025-07-10 21:46:01 +0900

    #8 [fix]: 공통응답, 에러처리

--- src/modules/user/user.controller.js
+++ src/modules/user/user.controller.js
@@ -1,23 +1,40 @@
 import { userService } from './user.service.js';
+import {
+  AuthUpdateSuccess,
+  AuthLoginSuccess,
+} from '../../utils/success.js';
+import {
+  AuthUpdateFailedError,
+} from '../../utils/error.js';
 
-export const updateNickname = async (req, res) => {
+/**
+ * 닉네임 변경 (PATCH /user/auth/signup)
+ */
+export const updateNickname = async (req, res, next) => {
   try {
     const userId = req.user.userId;
     const { nickname } = req.body;
 
     if (!nickname) {
-      return res.status(400).json({ message: '닉네임은 필수입니다.' });
+      throw new AuthUpdateFailedError("닉네임은 필수입니다.");
     }
 
     const updatedUser = await userService.updateNickname(userId, nickname);
 
-    res.json({
-      message: '닉네임이 수정되었습니다.',
-      user: updatedUser
-    });
+    res.status(200).json(new AuthUpdateSuccess(updatedUser));
   } catch (err) {
-    console.error('닉네임 수정 오류:', err);
-    res.status(500).json({ message: '서버 오류' });
+    next(err); // 에러 미들웨어로 넘김
   }
 };
 
+/**
+ * 카카오 콜백 로그인 처리 (/user/auth/kakao/callback)
+ */
+export const kakaoCallback = (req, res, next) => {
+  try {
+    const { token, user } = req.user;
+    res.status(200).json(new AuthLoginSuccess({ token, user }));
+  } catch (err) {
+    next(err);
+  }
+};
