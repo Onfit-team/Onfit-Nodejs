239354fc930facd261356eeafe89d01156d6f808
yujeong <dbwjda0201@naver.com>
2025-07-14 11:32:16 +0900

    [feat]: 옷장 아이템 상세 조회 API

---
+++ src/modules/wardrobe/wardrobe.repository.js
@@ -0,0 +1,23 @@
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+export const findItemById = async (userId, itemId) => {
+  return await prisma.item.findFirst({
+    where: {
+      id: itemId,
+      userId: userId,
+      isDeleted: false,
+    },
+    select: {
+      id: true,
+      category: true,
+      subcategory: true,
+      brand: true,
+      color: true,
+      size: true,
+      season: true,
+      purchaseDate: true,
+      image: true,
+    },
+  });
+};
