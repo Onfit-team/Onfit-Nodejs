a2b051a023918f75ad8aa565872310d988f2810b
chaeyeon089 <chaeyeon0091@naver.com>
2025-07-18 01:20:27 +0900

    #21[feat]:좌표기반 현재위치 정보 저장

--- src/modules/location/location.controller.js
+++ src/modules/location/location.controller.js
@@ -1,6 +1,5 @@
-
 import { validateSearchQuery } from "./location.dto.js";
-import { searchLocationService, saveLocationService } from "./location.service.js";
+import { searchLocationService, saveLocationService, getMyLocationService, saveCurrentLocationService } from "./location.service.js";
 import { OkSuccess, CreatedSuccess } from "../../utils/success.js";
 import { InvalidInputError } from "../../utils/error.js";
 
@@ -27,5 +26,27 @@ export const saveLocation = async (req, res, next) => {
     res.status(201).json(new CreatedSuccess(result));
   } catch (error) {
     next(error);
+  }
+};
+
+export const getMyLocation = async (req, res, next) => {
+  try {
+    const userId = req.user.userId;
+    const locationDTO = await getMyLocationService(userId);
+    res.status(200).json(new OkSuccess(locationDTO));
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const saveCurrentLocation = async (req, res, next) => {
+  try {
+    const { latitude, longitude } = req.body;
+    const userId = req.user.userId;
+
+    const result = await saveCurrentLocationService(userId, latitude, longitude);
+    res.status(201).json(new CreatedSuccess(result));
+  } catch (err) {
+    next(err);
   }
 };
--- src/routes/location.route.js
+++ src/routes/location.route.js
@@ -1,8 +1,11 @@
 import express from "express";
-import { searchLocation, saveLocation } from "../modules/location/location.controller.js";
+import { saveCurrentLocation, searchLocation, saveLocation, getMyLocation } from "../modules/location/location.controller.js";
 import authenticateJWT from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 router.get("/search", searchLocation);
 router.post("/save", authenticateJWT, saveLocation);
+router.get("/my", authenticateJWT, getMyLocation);
+router.post("/current", authenticateJWT, saveCurrentLocation);
+
 export default router;
