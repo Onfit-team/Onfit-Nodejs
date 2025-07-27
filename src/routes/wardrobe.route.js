3e06f5340bd57b322caa9d07cbf07232a8f364c1
yujeong <dbwjda0201@naver.com>
2025-07-27 20:22:47 +0900

    #54[feat]:아이템 검색( 필터: 계절, 색, 스타일태그, 브랜드)

--- src/routes/wardrobe.route.js
+++ src/routes/wardrobe.route.js
@@ -5,7 +5,8 @@ import {
   getWardrobeItemDetail,
   getWardrobeItemsByCategoryController,
   uploadWardrobeImage,
-  deleteWardrobeItemController
+  deleteWardrobeItemController,
+  getWardrobeItemsByFilterController
 } from '../modules/wardrobe/wardrobe.controller.js';
 import { authenticateJWT } from '../middlewares/auth.middleware.js';
 import multer from 'multer';
@@ -16,6 +17,7 @@ const router = express.Router();
 
 router.get('/items', authenticateJWT, getWardrobeItemsController);
 router.get('/items/categories', authenticateJWT, getWardrobeItemsByCategoryController);
+router.get('/items/filter', authenticateJWT, getWardrobeItemsByFilterController);
 router.get('/items/:itemId', authenticateJWT, getWardrobeItemDetail);
 router.post('/items', authenticateJWT, upload.single('image'), uploadWardrobeImage);
 router.delete('/items/:itemId', authenticateJWT, deleteWardrobeItemController);
