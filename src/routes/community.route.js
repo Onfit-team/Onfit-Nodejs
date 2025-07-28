21fedd8d4a2fd338a5d6caf7e79f52eac20ad14b
yujeong <dbwjda0201@naver.com>
2025-07-29 00:54:15 +0900

    #68[feat]:스타일 태그 검색 필터-최신 등록순, 인기순

--- src/routes/community.route.js
+++ src/routes/community.route.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import {
     toggleOutfitLikeController,
+    getPublishedOutfitsByOutfitTagsController
 } from '../modules/community/community.controller.js';
 import { authenticateJWT } from '../middlewares/auth.middleware.js';
 
@@ -8,5 +9,6 @@ const router = express.Router();
 
 // 게시글 하트 누르기 / 취소
 router.post('/outfits/:outfitId/like', authenticateJWT, toggleOutfitLikeController);
+router.get('/outfits/filter/outfit-tags', authenticateJWT, getPublishedOutfitsByOutfitTagsController);
 
 export default router;
