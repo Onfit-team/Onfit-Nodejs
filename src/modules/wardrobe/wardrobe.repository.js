b82bad71273b4b38a13792396fc244b63be14377
chaeyeon089 <chaeyeon0091@naver.com>
2025-07-30 01:58:26 +0900

    #74[feat]: 아이템 수정

--- src/modules/wardrobe/wardrobe.repository.js
+++ src/modules/wardrobe/wardrobe.repository.js
@@ -34,3 +34,43 @@ export const createItemTags = async (itemId, tagIds) => {
 
   return await prisma.itemTag.createMany({ data: itemTags });
 };
+
+// 아이템 수정
+export const updateItem = async (itemId, data) => {
+  const { tagIds, ...updateFields } = data;
+
+  return await prisma.item.update({
+    where: { id: parseInt(itemId) },
+    data: updateFields,
+  });
+};
+
+// 기존 태그 삭제
+export const clearItemTags = async (itemId) => {
+  return await prisma.itemTag.deleteMany({
+    where: { itemId: parseInt(itemId) },
+  });
+};
+
+
+export const findItemByItemId = async (itemId, userId) => {
+  return await prisma.item.findFirst({
+    where: {
+      id: itemId,
+      userId,
+      isDeleted: false,
+    },
+    select: {
+      id: true,
+      userId: true,
+      category: true,
+      subcategory: true,
+      brand: true,
+      color: true,
+      size: true,
+      season: true,
+      purchaseDate: true,
+      image: true,
+    },
+  });
+};
